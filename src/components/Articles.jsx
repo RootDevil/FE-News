f1e2ccb0edd749ddf0cde11b4e28ba771fed271c
Jonathon Coolledge <jonathon.coolledge@gmail.com>
2022-03-07 14:52:22 +0000

    Add article card component

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,11 +1,29 @@
+import { useEffect, useState } from "react";
 import * as api from "../utils/api";
+import ArticleCard from "./ArticleCard";
 
 const Articles = () => {
-    return (
-        <section className="Articles">
-            
-        </section>
-    )
-}
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    api.fetchArticles().then((articles) => {
+      setArticles(articles);
+    });
+  }, []);
+
+  return (
+    <section className="Articles">
+      {articles.map((article, index) => {
+        return <ArticleCard 
+            key={`article-${index}`} 
+            title={article.title}
+            topic={article.topic}
+            author={article.author}
+            votes={article.votes}
+            comment_count={article.comment_count} />;
+      })}
+    </section>
+  );
+};
 
 export default Articles;
