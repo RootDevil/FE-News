018230ac4b5ca12908fdab6ba89d8fb8b5e40e7a
Jonathon Coolledge <jonathon.coolledge@gmail.com>
2022-03-08 16:09:34 +0000

    Add user context

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Articles from "./components/Articles";
 import Header from "./components/Header";
 import SingleArticle from "./components/SingleArticle";
+import { UserContext } from "./contexts/UserContext";
 
 const theme = createTheme({
   palette: {
@@ -11,22 +13,28 @@ const theme = createTheme({
       main: "#455d7a",
     },
     secondary: {
-      main: "#455d7a"
-    }
+      main: "#455d7a",
+    },
   },
 });
 
 function App() {
+  const [user, setUser] = useState({
+    username: "grumpy19",
+  });
+
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Articles />} />
-          <Route path="/topics/:topic_slug" element={<Articles />} />
-          <Route path="/articles/:article_id" element={<SingleArticle />} />
-        </Routes>
-      </div>
+      <UserContext.Provider value={{ user, setUser }}>
+        <div className="App">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Articles />} />
+            <Route path="/topics/:topic_slug" element={<Articles />} />
+            <Route path="/articles/:article_id" element={<SingleArticle />} />
+          </Routes>
+        </div>
+      </UserContext.Provider>
     </ThemeProvider>
   );
 }
