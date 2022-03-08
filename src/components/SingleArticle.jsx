b17654f4cf210c47f648d37cc62ed0e52c268e2b
Jonathon Coolledge <jonathon.coolledge@gmail.com>
2022-03-08 13:35:35 +0000

    Refactor loading state

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -1,12 +1,13 @@
 import { Button } from "@mui/material";
 import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import * as api from "../utils/api";
 
 const SingleArticle = () => {
   const [article, setArticle] = useState({});
   const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
   const { article_id } = useParams();
 
   useEffect(() => {
@@ -17,30 +18,32 @@ const SingleArticle = () => {
     });
   }, []);
 
+  if (isLoading) return <p>Loading...</p>;
+
   return (
     <section className="Section-Single-Article">
       <header className="Header-Section-Title">
         <h2>{article.title}</h2>
         <span className="Article-title-span">
-          <Link to="/">
-            <Button variant="text" startIcon={<KeyboardBackspaceIcon />}>
-              Back
-            </Button>
-          </Link>
+          <Button
+            onClick={() => {
+              navigate(-1);
+            }}
+            variant="text"
+            startIcon={<KeyboardBackspaceIcon />}
+          >
+            Back
+          </Button>
           | <Link to={`/topics/${article.topic}`}>{article.topic}</Link>
         </span>
       </header>
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <section className="Article-body">
-          <span>
-            <i>{article.author}</i> |{" "}
-            {new Date(article.created_at).toString().substring(0, 15)}
-          </span>
-          <p className="Article-section-body">{article.body}</p>
-        </section>
-      )}
+      <section className="Article-body">
+        <span>
+          <i>{article.author}</i> |{" "}
+          {new Date(article.created_at).toString().substring(0, 15)}
+        </span>
+        <p className="Article-section-body">{article.body}</p>
+      </section>
     </section>
   );
 };
