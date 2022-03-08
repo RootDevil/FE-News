fe2796d0d9528ca173f7ff8f4ca480ade707ae20
Jonathon Coolledge <jonathon.coolledge@gmail.com>
2022-03-08 15:45:32 +0000

    Add handleVotes function to optimistically render vote count

--- src/components/InteractionPanel.jsx
+++ src/components/InteractionPanel.jsx
@@ -1,16 +1,28 @@
-import ThumbUpOutlinedIcon from '@mui/icons-material/ThumbUpOutlined';
-import ThumbDownAltOutlinedIcon from '@mui/icons-material/ThumbDownAltOutlined';
+import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined";
+import ThumbDownAltOutlinedIcon from "@mui/icons-material/ThumbDownAltOutlined";
+import { IconButton } from "@mui/material";
+import { useState } from "react";
 
 const InteractionPanel = ({ votes }) => {
-    return (
-        <section>
-            <span className="Span-interaction-panel">
-                <ThumbUpOutlinedIcon fontSize="medium"/>
-                <h4>{votes}</h4>
-                <ThumbDownAltOutlinedIcon fontSize="medium"/>
-            </span>
-        </section>
-    )
-}
+    const [articleVotes, setArticleVotes] = useState(votes);
+
+    const handleVotes = (votecrement) => {
+        setArticleVotes((currentVotes) => currentVotes + votecrement);
+    }
+
+  return (
+    <section>
+      <span className="Span-interaction-panel">
+        <IconButton aria-label="like" onClick={() => handleVotes(1)}>
+          <ThumbUpOutlinedIcon fontSize="medium" />
+        </IconButton>
+        <h4>{articleVotes}</h4>
+        <IconButton aria-label="dislike" onClick={() => handleVotes(-1)}>
+          <ThumbDownAltOutlinedIcon fontSize="medium" />
+        </IconButton>
+      </span>
+    </section>
+  );
+};
 
 export default InteractionPanel;
