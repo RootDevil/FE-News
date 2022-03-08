7a8bce9a1f3a036ff32ae417bd6d10d211f80977
Jonathon Coolledge <jonathon.coolledge@gmail.com>
2022-03-08 12:54:27 +0000

    Add loading state

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -1,16 +1,19 @@
 import { Button } from "@mui/material";
-import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
+import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import * as api from "../utils/api";
 
 const SingleArticle = () => {
   const [article, setArticle] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
   const { article_id } = useParams();
 
   useEffect(() => {
+    setIsLoading(true);
     api.fetchArticleById(article_id).then((article) => {
       setArticle(article);
+      setIsLoading(false);
     });
   }, []);
 
@@ -27,12 +30,17 @@ const SingleArticle = () => {
           | <Link to={`/topics/${article.topic}`}>{article.topic}</Link>
         </span>
       </header>
-      <section className="Article-body">
-        <span>
-          <i>{article.author}</i> | {new Date(article.created_at).toString().substring(0, 15)}
-        </span>
-        <p className="Article-section-body">{article.body}</p>
-      </section>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <section className="Article-body">
+          <span>
+            <i>{article.author}</i> |{" "}
+            {new Date(article.created_at).toString().substring(0, 15)}
+          </span>
+          <p className="Article-section-body">{article.body}</p>
+        </section>
+      )}
     </section>
   );
 };
