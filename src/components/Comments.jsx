760c2154fcb830c2215e16f1cb16d3f351972423
Jonathon Coolledge <jonathon.coolledge@gmail.com>
2022-03-09 11:19:33 +0000

    Add Comments component

---
+++ src/components/Comments.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+const Comments = () => {
+    const [comments, setComments] = useState([]);
+
+    useEffect(() => {
+
+    }, [])
+
+    return (
+        <section>
+
+        </section>
+    )
+}
+
+export default Comments;
