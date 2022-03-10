54eb1c45aa0f0e6f14a6710ea5bfacefe9cae5a2
Jonathon Coolledge <jonathon.coolledge@gmail.com>
2022-03-10 10:20:06 +0000

    Add validation

--- src/components/AddComment.jsx
+++ src/components/AddComment.jsx
@@ -4,20 +4,28 @@ import { UserContext } from "../contexts/UserContext";
 
 const AddComment = ({ setComments }) => {
   const [commentValue, setCommentValue] = useState("");
+  const [error, setError] = useState(false);
   const { user } = useContext(UserContext);
 
   const handleChange = ({ target: { value } }) => {
     setCommentValue(value);
+    setError(commentValue.length === 0);
   };
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (commentValue.length !== 0) {
       const commentToAdd = {
-          author: user.username,
-          body: commentValue,
-          votes: 0,
-          created_at: Date.now()
-      }
-    setComments((currentComments) => [commentToAdd, ...currentComments]);
+        author: user.username,
+        body: commentValue,
+        votes: 0,
+        created_at: Date.now(),
+      };
+      setComments((currentComments) => [commentToAdd, ...currentComments]);
+      setCommentValue("");
+    } else {
+      setError(true);
+    }
   };
 
   return (
@@ -26,11 +34,15 @@ const AddComment = ({ setComments }) => {
         <TextField
           multiline
           fullWidth
+          maxRows={3}
+          error={error}
+          helperText={error ? "Comment cannot be empty" : null}
           variant="filled"
           label="Add a comment..."
           placeholder="What's on your mind?"
           value={commentValue}
           onChange={handleChange}
+          onBlur={() => setError(commentValue.length === 0)}
         />
         <Button type="submit">Post</Button>
       </form>
