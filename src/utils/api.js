0d64d6aa507e8008d8c328cf81d40578aafc6848
Jonathon Coolledge <jonathon.coolledge@gmail.com>
2022-03-11 15:34:52 +0000

    Remove log

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const fetchArticles = (topic, sort, order) => {
-  console.log(sort);
   return axios
     .get("https://slurpy-nc-news.herokuapp.com/api/articles", {
       params: {
